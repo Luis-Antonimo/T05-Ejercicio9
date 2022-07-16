499dec6d8221f46500f5450ece57b2c86128f4ac
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-16 22:30:11 +0200

    ok
    ok

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T05Ejercicio9
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = new int[5];
+            for (int i=0; i<array.Length; i++)
+            {
+                Console.WriteLine("Escriba el valor del array con posición " + i);
+                int valor = Convert.ToInt32(Console.ReadLine());
+                array[i] = valor;
+            }
+            Console.WriteLine("\nMostrando contenido del array: ");
+            foreach (int x in array)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
